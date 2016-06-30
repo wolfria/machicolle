8349ccf5bf2f6324dee86b30b56a9a636cccc9ef
wolfria <wolfria0606@gmail.com>
2016-07-01 02:20:53 +0900

    だいたい完成してきたかな

--- www/js/gallery.js
+++
@@ -1,4 +0,0 @@
-var appKey    = "7d758bab2fa15918ef95934198066ba0efefe02f5c536ef8c393492003fcb185";
-var clientKey = "00af2cc8af94ded0e13965204087831640c45f9af428d1c98da22967c26e1a82";
-var ncmb = new NCMB(appKey, clientKey);
-
--- www/js/ncmbmaps.js
+++ www/js/ncmbmaps.js
@@ -10,22 +10,62 @@ $(function(){
     });
 });
 
-    function snapPicture () {
-        navigator.camera.getPicture (onSuccess, onFail, 
-            { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
 
-        //A callback function when snapping picture is success.
-        function onSuccess (imageData) {
-            var image = document.getElementById ('picture');
-            image.src = "data:image/jpeg;base64," + imageData;
-        }
+//起動時にマップ読み込み
+window.addEventListener("load", function(){
+    var latitude = null;
+    var longitude = null;
+    CurrentPoint.distance = 5000; //検索距離を5kmに設定
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, option);
+    //位置情報を検索するクラスのNCMB.Objectを作成する
+    var SpotClass = NCMB.Object.extend("Spot");
 
-        //A callback function when snapping picture is fail.
-        function onFail (message) {
-            alert ('Error occured: ' + message);
+    //NCMB.Queryを作成
+    var query = new NCMB.Query(SpotClass);
+    //位置情報をもとに検索する条件を設定
+    var geoPoint = new NCMB.GeoPoint(current.geopoint.latitude,current.geopoint.longitude);
+    query.withinKilometers("geo", geoPoint, current.distance);
+
+    //mobile backend上のデータ検索を実行する
+    query.find({
+        success: function(points) {
+        // 検索が成功した場合の処理
+
+        //Google mapの設定
+        var mapOptions = {
+              //中心地設定
+              center: new google.maps.LatLng(current.geopoint.latitude,current.geopoint.longitude),
+              //ズーム設定
+              zoom: 15,
+              //地図のタイプを指定
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        //idがmapのところにGoogle mapを表示
+        var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+
+            for (var i = 0; i < points.length; i++){
+                var point = points[i];
+                console.log("<p>画像の名前：" + point.get("name") + "</p>");
+                //位置情報オブジェクトを作成
+                var location = point.get("geo");
+                var myLatlng = new google.maps.LatLng(location.latitude,location.longitude);
+    
+                //店舗名、位置情報、Google mapオブジェクトを指定してマーカー作成メソッドを呼び出し
+                markToMap(point.get("name"), myLatlng, map);
+            }
+        },
+        error: function(error) {
+            // 検索に失敗した場合の処理
+            console.log(error.message);
         }
     }
-   　 
+);
+    // 位置情報取得失敗時
+    function errorHandler(error){
+        mapDiv.innerHTML = error.message;
+    }
+}, true);
+
 //位置情報取得に成功した場合のコールバック
 var onSuccess = function(position){
     var current = new CurrentPoint();
@@ -88,14 +128,20 @@ function search(current){
 
         for (var i = 0; i < points.length; i++){
             var point = points[i];
-            console.log("<p>画像の名前：" + point.get("name") + "</p>");
+            console.log("<p>画像の名前：" + point.get("name") + point.get("memo") + point.get("fileName") + "</p>");
 
-            //位置情報オブジェクトを作成            
+            //データ内オブジェクトを作成
             var location = point.get("geo");
             var myLatlng = new google.maps.LatLng(location.latitude,location.longitude);
+            var filenamedayo = point.get("fileName");
+
+            //NCMBサーバーからファイルをダウンロード
+            var getFile =  new NCMB.File(filenamedayo);
+            var image_canvas = document.getElementById("showImage");
+            getFile.fetchImgSource(image_canvas);
 
             //店舗名、位置情報、Google mapオブジェクトを指定してマーカー作成メソッドを呼び出し
-            markToMap(point.get("name"), myLatlng, map);
+            markToMap(point.get("name"),point.get("memo"), myLatlng, map);
         }
         },
         error: function(error) {
@@ -109,11 +155,12 @@ function search(current){
 function saveSpot(){
     //位置情報が取得できたときの処理
     var onSuccess = function (location){
-        
+        var fileInput = $("#image-file")[0];
+        var file = fileInput.files[0];
+        var ncmbFile = new NCMB.File(Date.now() + file.name, file);
         //記事内容を取得
         var title = $("#name").val();
         var memo = $("#memo").val();
-
         //位置情報オブジェクトを作成
         var geoPoint = new NCMB.GeoPoint(location.coords.latitude, location.coords.longitude);
         
@@ -127,6 +174,7 @@ function saveSpot(){
         spot.set("memo",memo);
         //保存を実行★
         spot.save();
+        ncmbFile.save()
         //前のページに戻る
         myNavigator.popPage();
     }
@@ -145,16 +193,21 @@ function saveSpot(){
 　　}
 　　
     //マーカーを作成
-　　function markToMap(name, position, map){
+　　function markToMap(name , memo , position, map){
     var marker = new google.maps.Marker({
         position: position,
-        title:name
+        title:name,
+        comment:memo
     });
     marker.setMap(map);
     google.maps.event.addListener(marker, 'click', function() {
-        var infowindow = new google.maps.InfoWindow({
-            content:marker.title
-        });
+        var infoWindowOption = {
+            position : position, //座標
+            content : "<div id='infoWindow'>" + marker.title + "<br>" +
+              "<img id='showImage'/><br>" +
+              "<span>" + memo +"</span><div>"
+        };
+        var infowindow = new google.maps.InfoWindow(infoWindowOption);
         infowindow.open(map,marker);
     });
 }
